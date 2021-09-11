beca3762444c224f358d32e2b154d00c26a25e7b
Fabrício Rocha <fabriciormartins85@gmail.com>
2021-09-11 00:43:52 -0300

    Projetos criados e limpos

---
+++ backend/src/main/java/br/com/devsuperior/vendas/VendasBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasBackendApplication.class, args);
+	}
+
+}
