2a016f005354f3a0a2f1ebb1d49e301ca6b58fe7
Fabrício Rocha <fabriciormartins85@gmail.com>
2021-09-12 15:42:00 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,27 @@
+import { Seller } from './seller';
+
+export type Sales = {
+        id: number;
+        visited: number;
+        deals: number;
+        amount: number;
+        date:  string;
+        seller: Seller; 
+        
+}
+export type SalePage = {
+   content?: Sales[];
+   last: boolean;
+   totalPages: number;
+   totalElements: number;
+   size?: number;
+   number: number;
+   first: boolean;
+   numberOfElements?: number;
+   empty?: boolean;
+
+}
+
 export type SaleSum = {
     sellerName: string;
     sum: number
